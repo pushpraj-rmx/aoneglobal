21de6485e3bef51fa0fab19a92b4846502c814ca
Pushpraj Dwivedi <pushprajdwivedi001@gmail.com>
2024-07-26 15:18:38 +0530

    before-handover

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+import viteConfig from './vite.config.js';
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: viteConfig,
+  integrations: [react()]
+});
